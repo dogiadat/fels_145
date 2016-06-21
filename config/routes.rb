0f0a1f2953d25be823e2be7e3326991cb3db776d
Do Gia Dat <dogiadat@gmail.com>
2016-06-21 16:10:26 +0700

    task4-user-sign-up
    tast4-completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root "static_pages#home"
   get "help" => "static_pages#help"
   get "contact" => "static_pages#contact"
- 
+  get "signup" => "users#new"
+  resources :users, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
