34fa55141412e0c8a2a36a509a12e844a0bc9500
Hoang Lanh <hoanglanhtb@gmail.com>
2016-06-22 17:37:03 +0700

    task8-edit-category

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   include SessionsHelper
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = t "controllers.categories.erro1"
+      redirect_to login_url
+    end
+  end
 end
